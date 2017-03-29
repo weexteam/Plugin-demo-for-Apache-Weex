b0d64a59da2a11cd1efb96e3cb935d656caa0b54
moxun.ljf <moxun.ljf@alibaba-inc.com>
2017-03-29 15:15:48 +0800

    update sample activity

--- generators/app/templates/examples/android/app/src/main/java/org/weex/demo/MainActivity.java
+++ generators/app/templates/examples/android/app/src/main/java/org/weex/demo/MainActivity.java
@@ -3,11 +3,20 @@ package org.weex.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.alibaba.weex.plugin.loader.WeexPluginContainer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadPlugin();
+    }
+
+    //TODO: This is just an example
+    private void loadPlugin() {
+        WeexPluginContainer.loadAll(getApplicationContext());
     }
 }
