b9b1b152fe5ce2bcaafd2639b3f0c7f9f6ab64b4
erha19 <faterrole@gmail.com>
2018-03-01 20:53:00 +0800

    Update template

--- generators/app/templates/playground/browser/pluginInstall.js
+++
@@ -1,7 +0,0 @@
-import <%= upperCamelCaseName %> from "../../js/src";
-
-if (window.Weex) {
-  Weex.install(<%= upperCamelCaseName %>);
-} else if (window.weex) {
-  weex.install(<%= upperCamelCaseName %>);
-}
